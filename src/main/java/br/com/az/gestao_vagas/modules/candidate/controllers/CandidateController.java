ae50156cee609cace5daff39915161f604086170
andre.zambroni <contato.andrezambroni@gmail.com>
2024-07-11 17:42:35 -0300

    aula 1

--- src/main/java/br/com/az/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/az/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,8 +2,11 @@ package br.com.az.gestao_vagas.modules.candidate.controllers;
 
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.az.gestao_vagas.modules.candidate.CandidateEntity;
 
 @RestController
 @RequestMapping("/candidate")
@@ -11,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
     
     @PostMapping("/")
-    public void create() {
-        
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
     }
 }
