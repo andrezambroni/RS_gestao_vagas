cc98c469a57ea6d3c12669db11b454291cb1ff29
andre.zambroni <contato.andrezambroni@gmail.com>
2024-07-12 16:38:08 -0300

    formatacao da validacao

--- src/main/java/br/com/az/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/az/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -14,13 +14,13 @@ public class CandidateEntity {
     private UUID id;
     private String name;
     
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] nao deve contar espaco")
+    @Pattern(regexp = "\\S+", message = "O campo [username] nao deve contar espaco")
     private String username;
 
     @Email(message = "O campo [email] deve conter um email valido")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "a senha deve conter entre 10 a 100 caracteres")
     private String password;
     private String description;
     private String curriculum;
