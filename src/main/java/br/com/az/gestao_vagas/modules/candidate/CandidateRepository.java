a74e53748294cadd7b07e324fa4ae99ac6f1d599
andre.zambroni <contato.andrezambroni@gmail.com>
2024-07-19 18:13:18 -0300

    validation user exists

--- src/main/java/br/com/az/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/br/com/az/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -1,10 +1,13 @@
 package br.com.az.gestao_vagas.modules.candidate;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
     
-    
+    // pq e quando é bom usar Optional
+    Optional<CandidateEntity> findByUsernameOREmail(String username, String email);
+
 }
--- src/main/java/br/com/az/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/az/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.az.gestao_vagas.exceptions.UserFoundException;
 import br.com.az.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.az.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
@@ -20,6 +21,13 @@ public class CandidateController {
 
     @PostMapping("/")
     public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+
+        this.candidateRepository
+        .findByUsernameOREmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+        });
+
         return this.candidateRepository.save(candidateEntity);
     }
 }
